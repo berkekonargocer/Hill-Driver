601a05838ccbc6e16431488e45cc302606ae179c
Berke Konargöçer <berke.konargocer@gmail.com>
2023-04-02 11:13:02 +0300

    Player Character Head Joint
    Character head reacts to movement using hinge joint

--- Assets/Nojumpo/Scripts/VehicleController.cs
+++ Assets/Nojumpo/Scripts/VehicleController.cs
@@ -5,60 +5,34 @@ namespace Nojumpo
 {
     public class VehicleController : MonoBehaviour
     {
-        #region Fields
-
-        #region Components
-
+        [Header("COMPONENTS")]
         [SerializeField] private Rigidbody2D _frontTireRigidbody2D;
         [SerializeField] private Rigidbody2D _backTireRigidbody2D;
         private Rigidbody2D _vehicleRigidbody2D;
 
-        #endregion
-
-        #region Vehicle Movement Settings
-
-        [Header("Vehicle Movement Settings")]
+        [Header("VEHICLE MOVEMENT SETTINGS")]
         [SerializeField] private float _vehicleMovementSpeed = 150.0f;
         [SerializeField] private float _vehicleRotationSpeed = 300.0f;
-
         private Vector2 _moveInput = Vector2.zero;
-        #endregion
-
-        #endregion
 
 
-
-        #region Unity Methods
-
-        #region Awake
-
+        // ------------------------ UNITY BUILT-IN METHODS ------------------------
         private void Awake() {
             SetComponents();
         }
-
-        #endregion
-
-        #region Fixed Update
 
         private void FixedUpdate() {
             ApplyCarMovement();
         }
 
-        #endregion
 
-        #endregion
-
-
-        #region Custom Private Methods
-
-        #region Input Methods
-
+        // ------------------------ INPUT METHODS ------------------------
         private void OnMove(InputValue inputValue) {
             _moveInput = inputValue.Get<Vector2>();
         }
 
-        #endregion
 
+        // ------------------------ CUSTOM PRIVATE METHODS ------------------------
         private void SetComponents() {
             _vehicleRigidbody2D = GetComponent<Rigidbody2D>();
         }
@@ -68,7 +42,5 @@ namespace Nojumpo
             _backTireRigidbody2D.AddTorque(-_moveInput.y * _vehicleMovementSpeed * Time.fixedDeltaTime);
             _vehicleRigidbody2D.AddTorque(_moveInput.y * _vehicleRotationSpeed * Time.fixedDeltaTime);
         }
-
-        #endregion
     }
 }
