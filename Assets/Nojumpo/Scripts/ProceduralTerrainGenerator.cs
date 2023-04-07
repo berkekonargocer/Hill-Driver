617f3a8fb741e5bb5383daf1ed01cd53dd84c796
Berke Konargöçer <berke.konargocer@gmail.com>
2023-04-07 22:15:02 +0300

    ProceduralTerrainGenerator.cs
    _levelLength => _terrainLength
    GenerateLevel => GenerateTerrain

--- Assets/Nojumpo/Scripts/ProceduralTerrainGenerator.cs
+++ Assets/Nojumpo/Scripts/ProceduralTerrainGenerator.cs
@@ -9,7 +9,7 @@ namespace Nojumpo
         // -------------------------------- FIELDS --------------------------------
         [Header("GENERATION SETTINGS")]
         [SerializeField] private SpriteShapeController _spriteShapeController;
-        [SerializeField, Range(3.0f, 100.0f)] private int _levelLength = 50;
+        [SerializeField, Range(3.0f, 100.0f)] private int _terrainLength = 50;
         [SerializeField, Range(1.0f, 50.0f)] private float _xMultiplier = 2.0f;
         [SerializeField, Range(1.0f, 50.0f)] private float _yMultiplier = 2.0f;
         [SerializeField, Range(0.0f, 1.0f)] private float _curveSmoothness = 0.5f;
@@ -20,17 +20,17 @@ namespace Nojumpo
         // ------------------------ UNITY BUILT-IN METHODS ------------------------
         private void OnValidate() {
             _spriteShapeController.spline.Clear();
-            GenerateLevel();
+            GenerateTerrain();
         }
 
         // ------------------------ CUSTOM PRIVATE METHODS ------------------------
-        private void GenerateLevel() {
-            for (int i = 0; i < _levelLength; i++)
+        private void GenerateTerrain() {
+            for (int i = 0; i < _terrainLength; i++)
             {
                 _lastPosition = transform.position + new Vector3(i * _xMultiplier, Mathf.PerlinNoise(0, i * _noiseStep) * _yMultiplier);
                 _spriteShapeController.spline.InsertPointAt(i, _lastPosition);
 
-                if (i != 0 && i != _levelLength - 1)
+                if (i != 0 && i != _terrainLength - 1)
                 {
                     _spriteShapeController.spline.SetTangentMode(i, ShapeTangentMode.Continuous);
                     _spriteShapeController.spline.SetLeftTangent(i, Vector3.left * _xMultiplier * _curveSmoothness);
@@ -38,8 +38,8 @@ namespace Nojumpo
                 }
             }
 
-            _spriteShapeController.spline.InsertPointAt(_levelLength, new Vector3(_lastPosition.x, transform.position.y - _bottom));
-            _spriteShapeController.spline.InsertPointAt(_levelLength + 1, new Vector3(transform.position.x, transform.position.y - _bottom));
+            _spriteShapeController.spline.InsertPointAt(_terrainLength, new Vector3(_lastPosition.x, transform.position.y - _bottom));
+            _spriteShapeController.spline.InsertPointAt(_terrainLength + 1, new Vector3(transform.position.x, transform.position.y - _bottom));
         }
     }
 }
