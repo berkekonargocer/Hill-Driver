a0321bf28b2abfa94101ba572dc1b99bc58d1dfb
Berke Konargöçer <berke.konargocer@gmail.com>
2023-04-17 04:04:14 +0300

    GameManager.cs | Vehicle Movement Speed | Tire Friction
    Vehicle Movement Speed 40 => 24
    Tire Friction 2 => 3
    GameManager.cs => ResetVariables method, OutOfFuel method, OutOfFuelCoroutine coroutine, LevelCompleted coroutine, StartLevelCompletedCoroutine method

--- Assets/Nojumpo/Scripts/GameManager.cs
+++ Assets/Nojumpo/Scripts/GameManager.cs
@@ -1,23 +1,39 @@
+using Nojumpo.ScriptableObjects;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace Nojumpo.Managers
 {
-    public class GameManager : MonoBehaviour {
+    public class GameManager : MonoBehaviour
+    {
         // -------------------------------- FIELDS --------------------------------
         [Header("SINGLETON")]
         private static GameManager _instance;
         public static GameManager Instance { get { return _instance; } }
 
+        [Header("VEHICLE VARIABLES")]
+        [SerializeField] private FloatVariableSO _vehicleFuel;
+        private Rigidbody2D _vehicleRigidbody2D;
+
         [Header("GAME LEVEL VARIABLES")]
         private int _levelCount;
 
         [Header("GAME STATE VARIABLES")]
-        public static bool _isDead;
-        public static bool _isLevelCompleted;
+        private static bool _isOutOfFuelCoroutineCalled = false;
+        public static bool _isDead = false;
+        public static bool _isLevelCompleted = false;
 
 
         // ------------------------ UNITY BUILT-IN METHODS ------------------------
+        private void OnEnable() {
+            SceneManager.sceneLoaded += ResetVariables;
+        }
+
+        private void OnDisable() {
+            SceneManager.sceneLoaded -= ResetVariables;
+        }
+
         private void Awake() {
             InitializeSingleton();
             _levelCount = SceneManager.sceneCountInBuildSettings;
@@ -25,14 +41,19 @@ namespace Nojumpo.Managers
         }
 
         private void Update() {
+            //if (!_isOutOfFuelCoroutineCalled && !_isDead && _vehicleFuel.Value <= 0)
+            //{
+            //    StartCoroutine(OutOfFuel());
+            //}
+
             if (_isDead && Input.GetKeyDown(KeyCode.Return))
             {
                 RestartGame();
             }
-            
+
             if (!_isDead && _isLevelCompleted && Input.GetKeyDown(KeyCode.Return))
             {
-
+                // Next Level
             }
         }
 
@@ -49,6 +70,34 @@ namespace Nojumpo.Managers
             }
         }
 
+        private void ResetVariables(Scene scene, LoadSceneMode loadSceneMode) {
+            _vehicleRigidbody2D = GameObject.Find("Vehicle").GetComponent<Rigidbody2D>();
+            _isDead = false;
+            _isLevelCompleted = false;
+            _isOutOfFuelCoroutineCalled = false;
+            _vehicleFuel.Value = 1.0f;
+        }
+
+        private IEnumerator OutOfFuelCoroutine() {
+            _isOutOfFuelCoroutineCalled = true;
+
+            yield return new WaitForSeconds(3.0f);
+
+            if (!_isLevelCompleted)
+            {
+                OutOfFuel();
+            }
+        }
+
+        private void OutOfFuel() {
+            _isDead = true;
+            _vehicleRigidbody2D.bodyType = RigidbodyType2D.Static;
+        }
+
+        private IEnumerator LevelCompleted() {
+            yield return new WaitForSeconds(3.5f);
+            _isLevelCompleted = true;
+        }
 
         // ------------------------ CUSTOM PUBLIC METHODS ------------------------
         public void StartGame() {
@@ -61,10 +110,14 @@ namespace Nojumpo.Managers
         }
 
         public void GoToNextLevel() {
-            if (SceneManager.GetActiveScene().buildIndex + 1 > _levelCount) 
+            if (SceneManager.GetActiveScene().buildIndex + 1 > _levelCount)
                 return;
 
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        public void StartLevelCompletedCoroutine() {
+            StartCoroutine(LevelCompleted());
         }
     }
 }
