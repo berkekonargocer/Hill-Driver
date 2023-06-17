d0dd068861e0724b45e65367936d7303bf882cb0
Berke Konargöçer <berke.konargocer@gmail.com>
2023-06-17 21:10:07 +0300

    FinishFlag.cs

---
+++ Assets/Nojumpo/Scripts/FinishFlag.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+namespace Nojumpo
+{
+    public class FinishFlag : MonoBehaviour
+    {
+        // -------------------------------- FIELDS ---------------------------------
+
+
+        // ------------------------- UNITY BUILT-IN METHODS ------------------------
+        void OnEnable() {
+
+        }
+
+        void OnDisable() {
+
+        }
+
+        void Awake() {
+
+        }
+
+        void Start() {
+
+        }
+
+        void Update() {
+
+        }
+
+
+        // ------------------------- CUSTOM PRIVATE METHODS ------------------------
+
+
+
+        // ------------------------- CUSTOM PUBLIC METHODS -------------------------
+
+    }
+}
