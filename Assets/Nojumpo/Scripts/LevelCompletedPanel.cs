3c633be507aee35d70efc92c576c199f6d66e466
Berke Konargöçer <berke.konargocer@gmail.com>
2023-06-29 03:58:31 +0300

    LevelCompletedPanel

---
+++ Assets/Nojumpo/Scripts/LevelCompletedPanel.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+namespace Nojumpo
+{
+    public class LevelCompletedPanel : MonoBehaviour
+    {
+        // -------------------------------- FIELDS ---------------------------------
+
+
+        // ------------------------- UNITY BUILT-IN METHODS ------------------------
+        void OnEnable() {
+
+        }
+
+        void OnDisable() {
+
+        }
+
+        void Awake() {
+
+        }
+
+        void Start() {
+
+        }
+
+        void Update() {
+
+        }
+
+
+        // ------------------------- CUSTOM PRIVATE METHODS ------------------------
+
+
+
+        // ------------------------- CUSTOM PUBLIC METHODS -------------------------
+
+    }
+}
